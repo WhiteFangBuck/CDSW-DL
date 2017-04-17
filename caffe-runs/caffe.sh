77acbbde4387ef2f44ca9ed610fdd5a6e2e7e91d
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-04-16 19:22:59 -0700

    Create caffe.sh

---
+++ caffe-runs/caffe.sh
@@ -0,0 +1,13 @@
+spark2-submit --master yarn --deploy-mode client \
+--num-executors 3 \
+--files ./lenet_memory_solver.prototxt,./lenet_memory_train_test.prototxt \
+--conf spark.driver.extraLibraryPath="${LD_LIBRARY_PATH}" \
+--conf spark.executorEnv.LD_LIBRARY_PATH="${LD_LIBRARY_PATH}" \
+--class com.yahoo.ml.caffe.CaffeOnSpark \
+./caffe-grid-0.1-SNAPSHOT-jar-with-dependencies.jar \
+-train -features accuracy,loss \
+-label label \
+-conf lenet_memory_solver.prototxt \
+-connection ethernet \
+-model mnist.model \
+-output mnist_features_result

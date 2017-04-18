ce5e3ab0e4865f8babf2eee4e626fa8d21211058
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-04-17 17:32:15 -0700

    Create prepare-mnist.sh

---
+++ scripts/prepare-mnist.sh
@@ -0,0 +1,8 @@
+mkdir ${HOME}/mnist
+pushd ${HOME}/mnist >/dev/null
+curl -O "http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
+curl -O "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
+curl -O "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
+curl -O "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
+zip -r mnist.zip *
+popd >/dev/null
